26d1007a81ad8f39121b63d39d58367e358df41b
ErElectrical <94623283+ErElectrical@users.noreply.github.com>
2021-12-23 15:30:23 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace addressbook
+{
+    public class program
+    {
+        public static List<person> people = new List<person>();
+        // it is our address book where our data is being stored
+        static void Main(string[] args)
+        {
+            string command = " ";
+            while (command != "exit")
+            {
+                Console.Clear();
+                Console.WriteLine("enter your command ");
+                command = Console.ReadLine().ToLower();
+                switch(command)
+                {
+                    case "add":
+                        {
+
+                        }
+                }
+            }
+        }
+    }
+}
