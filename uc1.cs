89cf5e8bce40f79467894e9ff097a689b2b64e27
ErElectrical <94623283+ErElectrical@users.noreply.github.com>
2021-12-24 01:11:18 +0000

    Add files via upload

---
+++ uc1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbookmain
+{  
+    // this method will create different varible for our address book
+    public class uc1
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string[] Addresses { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string email { get; set; }
+    }
+
+}
